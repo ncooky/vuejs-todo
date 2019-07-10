cc5296252e9fbcd429d3bb397a8c8f4c600172d7
Oscar Cheung <github@oscarcheung.me>
2019-07-10 18:44:05 +0800

    Add component TaskItem

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="task-item">
+    <input type="checkbox" :id="taskIdWithPrefix" :checked="isCompleted"/>
+    <label class="content" :for="taskIdWithPrefix">{{ taskContent }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: {
+    taskId: String,
+    taskContent: String,
+    isCompleted: Boolean,
+  },
+  computed: {
+    taskIdWithPrefix: function() {
+      return `task-${this.taskId}`;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.task-item {
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TaskItem taskContent="It's task content" taskId="1" :isCompleted="true"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import TaskItem from '@/components/TaskItem.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    TaskItem,
   },
 };
 </script>
